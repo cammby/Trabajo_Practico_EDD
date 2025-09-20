acfb9d6cee36f2036e22779acc053c82c2be2ceb
cammby <158327105+cammby@users.noreply.github.com>
2025-09-20 12:27:32 -0300

    user.py

--- modulos/user.py
+++ modulos/user.py
@@ -18,17 +18,17 @@ class Usuario():
 
     def obtener_apellido(self):
         return self._apellido
-    def cambiar_nombre(self,nuevoApellido):
+    def cambiar_apellido(self,nuevoApellido):
         self._apellido = nuevoApellido
 
     def obtener_edad(self):
         return self._edad
-    def cambiar_nombre(self,NuevaEdad):
+    def cambiar_edad(self,NuevaEdad):
         self._edad = NuevaEdad
 
-    def obtener_contraseña(self):
+    def obtener_contrasenia(self):
         return self.__contrasenia
-    def cambiar_nombre(self,nuevaContra):
+    def cambiar_contrasenia(self,nuevaContra):
         self.__contrasenia = nuevaContra
 
 
